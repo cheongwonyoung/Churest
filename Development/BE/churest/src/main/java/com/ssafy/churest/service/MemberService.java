eaddc63109f1db9e8e39949b3c07c9f350eeacf0
HanSunHee <xoa1235@naver.com>
2023-04-24 16:43:33 +0900

    [BE][Feat]: merge 충돌 해결

--- Development/BE/churest/src/main/java/com/ssafy/churest/service/MemberService.java
+++ Development/BE/churest/src/main/java/com/ssafy/churest/service/MemberService.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
-//import com.ssafy.churest.dto.resp.LoginResponseDto;
+import com.ssafy.churest.dto.resp.LoginResponseDto;
 import com.ssafy.churest.entity.Member;
 import org.springframework.security.oauth2.client.registration.ClientRegistration;
 import org.springframework.security.oauth2.core.endpoint.OAuth2AccessTokenResponse;
@@ -18,7 +18,7 @@ import java.util.Map;
     public interface MemberService {
     List<MemberResponseDto.FriendSearchInfo> getSearchMemberList(String nickname, int memberId);
 
-//    LoginResponseDto login(String code) throws JsonProcessingException;
+    LoginResponseDto login(String code) throws JsonProcessingException;
     JsonNode getToken(String code, ClientRegistration provider) throws JsonProcessingException;
     MultiValueMap<String, String> tokenRequest(String code, ClientRegistration provider);
 
