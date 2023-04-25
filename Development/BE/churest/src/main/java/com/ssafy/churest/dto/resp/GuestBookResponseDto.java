877c0122d3e9ac7665bee3281a2cbfed5a5ba8aa
baebae <tjdus2033@navver.com>
2023-04-25 14:42:57 +0900

    [BE][Feat]: 방명록 전달 데이터 수정

--- Development/BE/churest/src/main/java/com/ssafy/churest/dto/resp/GuestBookResponseDto.java
+++ Development/BE/churest/src/main/java/com/ssafy/churest/dto/resp/GuestBookResponseDto.java
@@ -1,13 +1,8 @@
 package com.ssafy.churest.dto.resp;
 
 import com.ssafy.churest.entity.GuestBook;
-import com.ssafy.churest.entity.Member;
-import com.sun.istack.NotNull;
 import lombok.Builder;
 import lombok.Data;
-import org.hibernate.annotations.CreationTimestamp;
-
-import javax.persistence.*;
 import java.time.LocalDateTime;
 
 public class GuestBookResponseDto {
@@ -16,7 +11,6 @@ public class GuestBookResponseDto {
     @Builder
     public static class Info{
         private int guestBookId;
-        private MemberResponseDto.LittleInfo toMember;
         private MemberResponseDto.LittleInfo fromMember;
         private String content;
         private LocalDateTime createdTime;
@@ -24,7 +18,6 @@ public class GuestBookResponseDto {
         public static GuestBookResponseDto.Info fromEntity(GuestBook guestBook){
             return Info.builder()
                     .guestBookId(guestBook.getGuestBookId())
-                    .toMember(MemberResponseDto.LittleInfo.fromEntity(guestBook.getToMember()))
                     .fromMember(MemberResponseDto.LittleInfo.fromEntity(guestBook.getFromMember()))
                     .content(guestBook.getContent())
                     .createdTime(guestBook.getCreatedTime()).build();
