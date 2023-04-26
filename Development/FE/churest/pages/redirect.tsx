e3d0937b4ed5403b5af7afb5931903e49787d02c
chaelin <chaedev3@gmail.com>
2023-04-26 13:48:36 +0900

    [Feat][FE]: redirect.tsx 주석처리

--- Development/FE/churest/pages/redirect.tsx
+++ Development/FE/churest/pages/redirect.tsx
@@ -1,7 +1,7 @@
-import { useRouter } from 'next/router';
+// import { useRouter } from 'next/router';
 
 export default function redirect() {
-  const router = useRouter();
-  const code = router.query.code;
+  // const router = useRouter();
+  // const code = router.query.code;
   return <div>리다이렉트랑께요</div>;
 }
