243a4f66fb598c11af8accf29b0c23ed437fbea7
강윤주 <jjjoody799@gmail.com>
2023-04-28 17:45:30 +0900

    [FE][Feat] 마이페이지, 상점 api

--- Development/FE/churest/apis/mypage.ts
+++ Development/FE/churest/apis/mypage.ts
@@ -9,3 +9,21 @@ export const getMyBirdsList = (memberId: number) => {
 export const getMyInfo = (memberId: number) => {
   return instance.get(`/my-page/${memberId}`);
 };
+
+// 아바타 바꾸기
+export const modifyLetter = (info: { avatarId: number; memberId: number }) => {
+  return instance.put(`/my-page/avatar`, info);
+};
+
+// 닉네임 바꾸기
+export const modifyNickname = (info: {
+  memberId: number;
+  nickname: string;
+}) => {
+  return instance.put(`/my-page/nickname`, info);
+};
+
+// 태그 모아보기
+export const getTaggedTree = (memberId: number) => {
+  return instance.get(`/tag/${memberId}`);
+};
