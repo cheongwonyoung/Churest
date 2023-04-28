56c82ad4db5a2ba89b74d9f1a014742f448a7026
cheongwonyoung <kaka491@naver.com>
2023-04-28 15:00:26 +0900

    [BE][Fix]: 채팅 수정

--- Development/BE/churest/src/main/java/com/ssafy/churest/api/MessageController.java
+++ Development/BE/churest/src/main/java/com/ssafy/churest/api/MessageController.java
@@ -25,6 +25,6 @@ public class MessageController
             message.setMessage(message.getSender()+"님이 입장하였습니다.");
         }
         // roomId는 태그된 상대 id로. 광장은 몇으로..?
-        sendingOperations.convertAndSend("/sub/chat/room"+message.getRoomId(), message);
+        sendingOperations.convertAndSend("/sub/chat/room/"+message.getRoomId(), message);
     }
 }
