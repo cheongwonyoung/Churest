4e19fd49ad67b6077e144b392976a72d807819fc
강윤주 <jjjoody799@gmail.com>
2023-05-02 16:45:47 +0900

    [FE][Fix]: Carousel img -> Image 태그 수정

--- Development/FE/churest/components/churest/MyBird.tsx
+++ Development/FE/churest/components/churest/MyBird.tsx
@@ -1,22 +1,16 @@
-import Image from 'next/image';
-import birdImg from '@/public/assets/bird_1_img.png';
 import Carousel from '../common/Carousel';
 import { getMyBirdsList } from '@/apis/mypage';
-import { useEffect, useState, useRef } from 'react';
+import { useState } from 'react';
 import { useQuery } from 'react-query';
-// import { useRecoilValue } from 'recoil';
-// import { userStatus, userToken } from '../recoil/userAtom';
 
 export default function MyBird() {
   const cardType = 'mybird';
   const memberId: number = 1;
-  console.log(memberId + '번 멤버');
 
   // 나의 새 목록
   const [birdList, setMyBirds] = useState([{}]);
   useQuery('mybirds', () => getMyBirdsList(Number(memberId)), {
     onSuccess(data) {
-      console.log('우히히 나의 새들이다');
       console.log(data.data);
       setMyBirds([...data.data]);
     },
