d824405b7f07c8ac8b1f90b842d230cff53ad504
chaelin <chaedev3@gmail.com>
2023-05-02 17:34:04 +0900

    [DEV][Feat]: image 되는지 확인

--- Development/FE/churest/next.config.js
+++ Development/FE/churest/next.config.js
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "storage.cloud.google.com",
+      },
+      {
+        protocol: "http",
+        hostname: "news.samsungdisplay.com",
+      },
+
+
+    ],
+  },
 }
 
 module.exports = nextConfig
