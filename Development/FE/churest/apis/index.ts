0dfe4353536f92937b5e91971a2f09495c34190a
chaelin <chaedev3@gmail.com>
2023-05-02 15:59:40 +0900

    [DEV][Feat]: meta 태그 추가

--- Development/FE/churest/apis/index.ts
+++ Development/FE/churest/apis/index.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const instance = axios.create({
-  baseURL: 'http://k8a505.p.ssafy.io:8080/api',
+  baseURL: 'k8a505.p.ssafy.io:8080/api',
   headers: {
     'Content-Type': `application/json`,
   },
