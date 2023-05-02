b7bfb344f6bb6d2911d9200b9f3cb6e5501a3529
강윤주 <jjjoody799@gmail.com>
2023-05-02 16:47:18 +0900

    [FE][Feat]: 우편함 API 연결

---
+++ Development/FE/churest/components/churest/LetterSlide.tsx
@@ -0,0 +1,41 @@
+import Image from 'next/image';
+import frameImg from '@/public/assets/letter_img.png';
+
+type Props = {
+  letters: any;
+};
+
+export default function LetterBox({ letters }: Props) {
+  return (
+    <>
+      {letters.map((letter: any, idx: number) => {
+        return (
+          <div
+            key={idx}
+            className="letter blue-clay"
+            style={{
+              backgroundImage: 'url(@/public/assets/letter_img.png)',
+              width: '400px',
+              height: '550px',
+            }}
+          >
+            <Image src={frameImg} alt="" width={380} />
+            <div className="input">{letter.content}</div>
+          </div>
+        );
+      })}
+      <style jsx>{`
+        .letter {
+          position: relative;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        }
+        .input {
+          position: absolute;
+          width: 225px;
+        }
+      `}</style>
+    </>
+  );
+}
