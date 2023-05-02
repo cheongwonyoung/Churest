af002ad82c748f2015550433697d6f83a5466ba0
chaelin <chaedev3@gmail.com>
2023-05-02 16:14:39 +0900

    [DEV][Feat]: 수정

--- Development/FE/churest/apis/index.ts
+++ Development/FE/churest/apis/index.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const instance = axios.create({
-  baseURL: 'http://k8a505.p.ssafy.io:8080/api',
+  baseURL: 'https://k8a505.p.ssafy.io/api',
   headers: {
     'Content-Type': `application/json`,
   },
