a657170d18fb54638f79ee4fd0db19b99a429c4a
chaelin <chaedev3@gmail.com>
2023-05-02 15:08:48 +0900

    [DEV][Feat]: https -> http

--- Development/FE/churest/apis/index.ts
+++ Development/FE/churest/apis/index.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const instance = axios.create({
-  baseURL: 'https://k8a505.p.ssafy.io:8080/api',
+  baseURL: 'http://k8a505.p.ssafy.io:8080/api',
   headers: {
     'Content-Type': `application/json`,
   },
