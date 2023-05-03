1ae5bdc8fa0f3b4bf8728dc448f910e695e4e91c
cheongwonyoung <kaka491@naver.com>
2023-05-03 10:51:45 +0900

    [BE][Fix ]: jwt filter 적용

--- Development/BE/churest/src/main/java/com/ssafy/churest/config/JwtAuthenticationFilter.java
+++ Development/BE/churest/src/main/java/com/ssafy/churest/config/JwtAuthenticationFilter.java
@@ -1,17 +1,30 @@
 package com.ssafy.churest.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ssafy.churest.util.JwtTokenProvider;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.JwtException;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.GenericFilterBean;
+import org.springframework.web.server.ResponseStatusException;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@Component
+@Getter
+@Slf4j
 public class JwtAuthenticationFilter extends GenericFilterBean {
 
     private JwtTokenProvider jwtTokenProvider;
@@ -23,14 +36,44 @@ public class JwtAuthenticationFilter extends GenericFilterBean {
     // Request로 들어오는 Jwt Token의 유효성을 검증하는 filter를 filterChain에 등록합니다.
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+
+
         String token = jwtTokenProvider.resolveToken((HttpServletRequest) servletRequest);
         logger.info("come here !!!! "+ token);
-        if (token != null && jwtTokenProvider.validateToken(token)) {   // token 검증
-            logger.info("come here1 !!!! "+ token);
-            Authentication auth = jwtTokenProvider.getAuthentication(token);    // 인증 객체 생성
-            logger.info("come here2 !!!! "+ token);
-            SecurityContextHolder.getContext().setAuthentication(auth); // SecurityContextHolder에 인증 객체 저장
-            logger.info("come here3 !!!! "+ token);
+        if(token != null){
+            try{
+                jwtTokenProvider.validateToken(token);
+                // token 검증
+                logger.info("come here1 !!!! "+ token);
+                Authentication auth = jwtTokenProvider.getAuthentication(token);    // 인증 객체 생성
+                logger.info("come here2 !!!! "+ token);
+                SecurityContextHolder.getContext().setAuthentication(auth); // SecurityContextHolder에 인증 객체 저장
+                logger.info("come here3 !!!! "+ token);
+            }
+            catch(ExpiredJwtException jwtException){
+                logger.info("토큰 만료");
+
+                ObjectMapper mapper = new ObjectMapper();
+
+                HttpServletResponse response = (HttpServletResponse) servletResponse;
+                response.setStatus(HttpStatus.UNAUTHORIZED.value());
+                response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+                response.setCharacterEncoding("UTF-8");
+
+                ResponseStatusException responseStatusException = new ResponseStatusException(
+                        HttpStatus.UNAUTHORIZED, "토큰이 만료되었습니다.");
+
+                mapper.writeValue(response.getWriter(), responseStatusException);
+
+            }catch (Exception exception) {
+                logger.info("jwtException : 잘못된 토큰이 들어왔음");
+                throw exception;
+            }
+
+        }
+
+        else{
+            logger.info("?????");
         }
         filterChain.doFilter(servletRequest, servletResponse);
     }
--- Development/BE/churest/src/main/java/com/ssafy/churest/config/Swagger2Config.java
+++ Development/BE/churest/src/main/java/com/ssafy/churest/config/Swagger2Config.java
@@ -14,10 +14,15 @@ import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.schema.AlternateTypeRules;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.util.Arrays;
 import java.util.List;
 
 @Configuration
@@ -62,6 +67,22 @@ public class SwaggerConfig {
 
         @ApiModelProperty(value = "정렬(사용법: 컬럼명,ASC|DESC)")
         private List<String> sort;
+    }
+
+    // swagger에서 jwt 토큰값 넣기위한 설정
+    private ApiKey apiKey() {
+        return new ApiKey("JWT", "Authorization", "header");
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder().securityReferences(defaultAuth()).build();
+    }
+
+    private List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Arrays.asList(new SecurityReference("JWT", authorizationScopes));
     }
 
 }
