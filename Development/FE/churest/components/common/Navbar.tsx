2716920e6433880e77020c2688040b421cdd622b
강윤주 <jjjoody799@gmail.com>
2023-05-08 09:43:31 +0900

    [FE][Fix]: Navbar syntax error

--- Development/FE/churest/components/common/Navbar.tsx
+++ Development/FE/churest/components/common/Navbar.tsx
@@ -13,7 +13,6 @@ export default function Navbar() {
       <Link href={'/churest'}>츄레스트</Link>
       <Link href={'/memory'}> 추억생성하긔</Link>
       <Link href={'/signup'}> 회원가입</Link>
-      <Link href={'/memory'}> 추억생성하긔</Link>
       <Link href={'/chuworld'}>츄월드</Link>
     </div>
   );
