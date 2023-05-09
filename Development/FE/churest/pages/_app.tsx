b5397dde7528ef9472f330c28cef01790dea5404
chaelin <chaedev3@gmail.com>
2023-05-09 10:03:04 +0900

    [FE][Feat]: navbar & 스크롤 없애기

--- Development/FE/churest/pages/_app.tsx
+++ Development/FE/churest/pages/_app.tsx
@@ -1,16 +1,18 @@
-import Navbar from '@/components/common/Navbar';
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { RecoilRoot } from 'recoil';
+import Modals from '@/components/common/Modals';
 
 export default function App({ Component, pageProps }: AppProps) {
   const queryClient = new QueryClient();
+
   return (
     <>
       <QueryClientProvider client={queryClient}>
         <RecoilRoot>
-          <Navbar />
+          {/* <Navbar /> */}
+          <Modals />
           <Component {...pageProps} />{' '}
           <style jsx global>
             {``}
