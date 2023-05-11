6e368ee31ac8fb1467a04d5f668bfddf8fb9623b
강윤주 <jjjoody799@gmail.com>
2023-05-11 23:43:14 +0900

    [FE][Fix]: 방명록 조회 및 작성

--- Development/FE/churest/apis/letterbox.ts
+++ Development/FE/churest/apis/letterbox.ts
@@ -1,25 +1,23 @@
 import { instance } from '.';
 
-// 방명록 조회 
+// 방명록 조회
 export const getLetterList = (memberId: number) => {
   return instance.get(`/guest-book?memberId=${memberId}`);
 };
 
-// 방명록 작성 
-export const writeLetter = (
-  writeInfo: {
-    content: string;
-    fromMemberId: number;
-    toMemberId: number;
+// 방명록 작성
+export const writeLetter = (writeInfo: {
+  content: string;
+  fromMemberId: number;
+  toMemberId: number;
 }) => {
   return instance.post(`/guest-book`, writeInfo);
 };
 
-// 방명록 삭제 
-export const deleteLetter = (
-  deleteInfo: {
-    fromMemberId: number;
-    GuestBookId: number;
+// 방명록 삭제
+export const deleteLetter = (deleteInfo: {
+  fromMemberId: number;
+  guestBookId: number;
 }) => {
-  return instance.delete(`/guest-book`, {data:deleteInfo});
+  return instance.delete(`/guest-book`, { data: deleteInfo });
 };
