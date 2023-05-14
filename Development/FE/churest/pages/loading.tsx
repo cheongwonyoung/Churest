f6df706618c543a63d1db619f8552b57bdfda437
chaelin <chaedev3@gmail.com>
2023-05-14 22:22:25 +0900

    [FE][Feat]: login 페이지 없애고 index 페이지에 로그인 넣음

--- Development/FE/churest/pages/loading.tsx
+++ Development/FE/churest/pages/loading.tsx
@@ -2,5 +2,10 @@ import { images } from '@/public/assets/images';
 import Image from 'next/image';
 
 export default function Loading() {
-  return <Image src={images.loading} width={300} height={300} alt="" />;
+  return (
+    <div>
+      <Image src={images.loading} width={300} height={300} alt="" />;
+      <p>임시 로딩 페이쥐</p>
+    </div>
+  );
 }
