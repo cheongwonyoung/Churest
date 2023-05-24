9fc69ec19dc91b89e60a8a1c17c8c3861bc08348
10yutae29 <10yutae29@gmail.com>
2023-05-24 15:33:59 +0900

    [FE][Fix]:dynamic-Import

--- Development/FE/churest/components/churest/Options.tsx
+++ Development/FE/churest/components/churest/Options.tsx
@@ -1,31 +1,14 @@
-import { BirdHouse1 } from '../3DFiles/BirdHouse/BirdHouse1';
-import { BirdHouse2 } from '../3DFiles/BirdHouse/BirdHouse2';
-import { BirdHouse3 } from '../3DFiles/BirdHouse/BirdHouse3';
-import { Bird1 } from '../3DFiles/Birds/Bird1';
-import { Bird2 } from '../3DFiles/Birds/Bird2';
-import { Bird3 } from '../3DFiles/Birds/Bird3';
-import { Bird4 } from '../3DFiles/Birds/Bird4';
-import { Bird5 } from '../3DFiles/Birds/Bird5';
-import { Bird6 } from '../3DFiles/Birds/Bird6';
-import { Bird7 } from '../3DFiles/Birds/Bird7';
-import { House1 } from '../3DFiles/House/House_1';
-import { House2 } from '../3DFiles/House/House_2';
-import { House3 } from '../3DFiles/House/House_3';
-import { House4 } from '../3DFiles/House/House_4';
-import { House5 } from '../3DFiles/House/House_5';
-import { House6 } from '../3DFiles/House/House_6';
-import { Branch } from '../3DFiles/Trees/Branch';
-import { Seed } from '../3DFiles/Trees/Seed';
-import { Sprout } from '../3DFiles/Trees/Sprout';
-import { Tree1 } from '../3DFiles/Trees/Tree1';
-import { Tree2 } from '../3DFiles/Trees/Tree2';
-import { Tree3 } from '../3DFiles/Trees/Tree3';
-import { Tree4 } from '../3DFiles/Trees/Tree4';
-import { Tree5 } from '../3DFiles/Trees/Tree5';
-import { Tree6 } from '../3DFiles/Trees/Tree6';
-import { Tree7 } from '../3DFiles/Trees/Tree7';
-import { Tree8 } from '../3DFiles/Trees/Tree8';
-import { Tree9 } from '../3DFiles/Trees/Tree9';
+import dynamic from 'next/dynamic';
+
+const BirdHouse1 = dynamic(() =>
+  import('../3DFiles/BirdHouse/BirdHouse1').then((cp) => cp.BirdHouse1)
+);
+const BirdHouse2 = dynamic(() =>
+  import('../3DFiles/BirdHouse/BirdHouse2').then((cp) => cp.BirdHouse2)
+);
+const BirdHouse3 = dynamic(() =>
+  import('../3DFiles/BirdHouse/BirdHouse3').then((cp) => cp.BirdHouse3)
+);
 
 export function BirdHouses(id: number) {
   switch (id) {
@@ -37,6 +20,25 @@ export function BirdHouses(id: number) {
       return <BirdHouse1 />;
   }
 }
+
+const House1 = dynamic(() =>
+  import('../3DFiles/House/House_1').then((cp) => cp.House1)
+);
+const House2 = dynamic(() =>
+  import('../3DFiles/House/House_2').then((cp) => cp.House2)
+);
+const House3 = dynamic(() =>
+  import('../3DFiles/House/House_3').then((cp) => cp.House3)
+);
+const House4 = dynamic(() =>
+  import('../3DFiles/House/House_4').then((cp) => cp.House4)
+);
+const House5 = dynamic(() =>
+  import('../3DFiles/House/House_5').then((cp) => cp.House5)
+);
+const House6 = dynamic(() =>
+  import('../3DFiles/House/House_6').then((cp) => cp.House6)
+);
 
 export function Houses(id: number) {
   switch (id) {
@@ -54,6 +56,28 @@ export function Houses(id: number) {
       return <House6 />;
   }
 }
+
+const Bird1 = dynamic(() =>
+  import('../3DFiles/Birds/Bird1').then((cp) => cp.Bird1)
+);
+const Bird2 = dynamic(() =>
+  import('../3DFiles/Birds/Bird2').then((cp) => cp.Bird2)
+);
+const Bird3 = dynamic(() =>
+  import('../3DFiles/Birds/Bird3').then((cp) => cp.Bird3)
+);
+const Bird4 = dynamic(() =>
+  import('../3DFiles/Birds/Bird4').then((cp) => cp.Bird4)
+);
+const Bird5 = dynamic(() =>
+  import('../3DFiles/Birds/Bird5').then((cp) => cp.Bird5)
+);
+const Bird6 = dynamic(() =>
+  import('../3DFiles/Birds/Bird6').then((cp) => cp.Bird6)
+);
+const Bird7 = dynamic(() =>
+  import('../3DFiles/Birds/Bird7').then((cp) => cp.Bird7)
+);
 
 export function Birds(id: number) {
   switch (id) {
@@ -73,6 +97,44 @@ export function Birds(id: number) {
       return <Bird7 scale={0.5} />;
   }
 }
+
+const Tree1 = dynamic(() =>
+  import('../3DFiles/Trees/Tree1').then((cp) => cp.Tree1)
+);
+const Tree2 = dynamic(() =>
+  import('../3DFiles/Trees/Tree2').then((cp) => cp.Tree2)
+);
+const Tree3 = dynamic(() =>
+  import('../3DFiles/Trees/Tree3').then((cp) => cp.Tree3)
+);
+const Tree4 = dynamic(() =>
+  import('../3DFiles/Trees/Tree4').then((cp) => cp.Tree4)
+);
+const Tree5 = dynamic(() =>
+  import('../3DFiles/Trees/Tree5').then((cp) => cp.Tree5)
+);
+const Tree6 = dynamic(() =>
+  import('../3DFiles/Trees/Tree6').then((cp) => cp.Tree6)
+);
+const Tree7 = dynamic(() =>
+  import('../3DFiles/Trees/Tree7').then((cp) => cp.Tree7)
+);
+const Tree8 = dynamic(() =>
+  import('../3DFiles/Trees/Tree8').then((cp) => cp.Tree8)
+);
+const Tree9 = dynamic(() =>
+  import('../3DFiles/Trees/Tree9').then((cp) => cp.Tree9)
+);
+
+const Seed = dynamic(() =>
+  import('../3DFiles/Trees/Seed').then((cp) => cp.Seed)
+);
+const Sprout = dynamic(() =>
+  import('../3DFiles/Trees/Sprout').then((cp) => cp.Sprout)
+);
+const Branch = dynamic(() =>
+  import('../3DFiles/Trees/Branch').then((cp) => cp.Branch)
+);
 
 export function Trees(treeInfo: {
   boardId: number;
